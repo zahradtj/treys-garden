f271db1c405c2565ea76ecbabac0596e39138b20
zahradtj <trey.j.zahradka@gmail.com>
2026-06-18 21:51:02 -0500

    wip

--- astro.config.ts
+++ astro.config.ts
@@ -7,21 +7,11 @@ import { loadEnv } from "vite";
 import spectre, { type GiscusMapping } from "./package/src";
 import { spectreDark } from "./src/ec-theme";
 
-const {
-	GISCUS_REPO,
-	GISCUS_REPO_ID,
-	GISCUS_CATEGORY,
-	GISCUS_CATEGORY_ID,
-	GISCUS_MAPPING,
-	GISCUS_STRICT,
-	GISCUS_REACTIONS_ENABLED,
-	GISCUS_EMIT_METADATA,
-	GISCUS_LANG,
-} = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
+const {} = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
 
 // https://astro.build/config
 const config = defineConfig({
-	site: "https://spectre.lou.gg",
+	site: "https://zahradka.ninja",
 	output: "static",
 	integrations: [
 		expressiveCode({
@@ -30,30 +20,19 @@ const config = defineConfig({
 		mdx(),
 		sitemap(),
 		spectre({
-			name: "Spectre",
+			name: "Trey's Garden",
 			openGraph: {
 				home: {
-					title: "Spectre",
-					description: "A minimalistic theme for Astro.",
+					title: "Trey's Garden",
+					description: "A site full of Trey's hobbies and thoughts.",
 				},
 				blog: {
 					title: "Blog",
-					description: "News and guides for Spectre.",
+					description: "News and guides.",
 				},
 				projects: {
 					title: "Projects",
 				},
-			},
-			giscus: {
-				repository: GISCUS_REPO,
-				repositoryId: GISCUS_REPO_ID,
-				category: GISCUS_CATEGORY,
-				categoryId: GISCUS_CATEGORY_ID,
-				mapping: GISCUS_MAPPING as GiscusMapping,
-				strict: GISCUS_STRICT === "true",
-				reactionsEnabled: GISCUS_REACTIONS_ENABLED === "true",
-				emitMetadata: GISCUS_EMIT_METADATA === "true",
-				lang: GISCUS_LANG,
 			},
 		}),
 	],
